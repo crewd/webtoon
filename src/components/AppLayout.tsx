a86eb66540a4db04dc89e4ee0513d07abe632ab4
crewd <newspaper321@nate.com>
2022-06-15 20:30:02 +0900

    Add header

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -2,9 +2,17 @@ import React from "react";
 
 const AppLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <div className="bg-blue-500">
-      <header>헤더</header>
-      <main>{children}</main>
+    <div className="flex flex-col items-center">
+      <header className="pt-[20px] pb-[20px] lg:w-[1024px] flex flex-col items-center">
+        <span className="text-center mb-[30px] w-full">
+          <h1 className="text-3xl font-bold">Webtoon</h1>
+        </span>
+        <input
+          className="border border-slate-400 w-full outline-none p-2"
+          placeholder="작가 / 작품을 입력하세요"
+        />
+      </header>
+      <main className="lg:w-[1024px]">{children}</main>
     </div>
   );
 };
