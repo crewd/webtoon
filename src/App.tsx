6aa4f3f2d8091bb6ccd13eb8ba4a3dee27821939
crewd <newspaper321@nate.com>
2022-06-15 18:54:04 +0900

    fix: eslint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AppLayout from "./components/AppLayout";
+import "./style/index.css";
 
 function App() {
   return <AppLayout>메인</AppLayout>;
--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AppLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <div className="bg-black">
+    <div className="bg-blue-500">
       <header>헤더</header>
       <main>{children}</main>
     </div>
