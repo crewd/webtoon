08914233291f8c076d1ec1986df54d04bd97a534
crewd <newspaper321@nate.com>
2022-06-17 05:42:03 +0900

    change layout style

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const AppLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
     <div className="flex flex-col items-center">
-      <header className="pt-[20px] pb-[20px] md:w-[768px] w-full flex flex-col items-center bg-white">
+      <header className="pt-[20px] pb-[20px] md:w-[768px] w-[90%] flex flex-col items-center bg-white">
         <span className="text-center mb-[30px] w-full">
           <h1 className="text-3xl font-bold">Webtoon</h1>
         </span>
@@ -13,7 +13,7 @@ const AppLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
         />
       </header>
       <main className="w-full flex justify-center">
-        <div className="md:w-[768px] w-full">{children}</div>
+        <div className="md:w-[768px] w-[90%]">{children}</div>
       </main>
     </div>
   );
