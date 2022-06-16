07d78c219aca846195253ebc609b2b7a11ddf653
crewd <newspaper321@nate.com>
2022-06-16 20:14:26 +0900

    change skeleton style

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -4,7 +4,7 @@ const Skeleton: React.FC = () => {
   return (
     <div className="w-1/2 sm:w-1/3 md:w-1/4 p-3 md:h-[210.25px] sm:h-[229.83px] h-[229.06px] animate-pulse ">
       <div className="w-full bg-gray-300 rounded-t-md h-[calc(100%-25px)]"></div>
-      <div className="text-md text-center rounded-b-md bg-white border border-gray-300 text-white p-1 flex justify-center">
+      <div className="text-md text-center rounded-b-md bg-white border border-gray-300 text-white p-2 flex justify-center">
         <div className="w-4/5 bg-gray-300 h-[15px]"></div>
       </div>
     </div>
