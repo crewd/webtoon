9f16a00e944ce9da68fd0e2e969910e28ebb16e2
crewd <newspaper321@nate.com>
2022-06-17 00:06:55 +0900

    Add card hover effect

--- src/components/WebtoonCard.tsx
+++ src/components/WebtoonCard.tsx
@@ -5,16 +5,18 @@ const WebtoonCard: React.FC<{ webtoonInfo: webtoonData }> = ({
   webtoonInfo,
 }) => {
   return (
-    <div className="w-1/2 sm:w-1/3 md:w-1/4 p-3">
-      <div>
-        <img
-          src={webtoonInfo.img}
-          alt={webtoonInfo.title}
-          className="w-full rounded-t-md"
-        />
-      </div>
-      <div className="text-md text-center rounded-b-md bg-zinc-800 text-white p-1">
-        <p>{webtoonInfo.title}</p>
+    <div className="w-1/2 sm:w-1/3 p-3 md:w-1/4 rounded-md cursor-pointer ">
+      <div className="hover:shadow-2xl hover:scale-110 z-99">
+        <div>
+          <img
+            src={webtoonInfo.img}
+            alt={webtoonInfo.title}
+            className="w-full rounded-t-md"
+          />
+        </div>
+        <div className="text-md text-center rounded-b-md bg-zinc-800 text-white p-1">
+          <p>{webtoonInfo.title}</p>
+        </div>
       </div>
     </div>
   );
