66759a1690d6ce1ccbfb4aa813ddbb1eee75c2a3
crewd <newspaper321@nate.com>
2022-06-17 16:44:58 +0900

    change modal style

--- src/components/WebtoonDetail.tsx
+++ src/components/WebtoonDetail.tsx
@@ -29,51 +29,51 @@ const WebtoonDetail: React.FC<{
         onClick={props.onModal}
       ></div>
 
-      <div className="w-fit h-fit p-[20px] bg-white fixed m-auto top-[50%] bottom-[50%] left-0 right-0 z-20 rounded-2xl">
+      <div className="w-fit h-fit p-[20px] bg-white fixed m-auto top-[50%] max-w-[90%] bottom-[50%] left-0 right-0 z-20 rounded-2xl">
         <button
-          className="absolute -top-5 -right-5 w-[30px] h-[30px] rounded-full bg-white/70 hover:bg-white"
+          className="absolute -top-3 -right-2 w-[30px] h-[30px] rounded-full bg-white/70 hover:bg-white"
           onClick={props.onModal}
         >
           <FontAwesomeIcon icon={faX} />
         </button>
         <div className="flex sm:flex-row flex-col sm:items-center">
-          <div className="md:mr-10">
+          <div className="sm:mr-10 w-full">
             <img
               src={props.webtoonInfomation?.img}
               alt={props.webtoonInfomation?._id}
               className="rounded-xl"
             />
           </div>
-          <div className="h-full p-[15px]  flex flex-col">
-            <div className="flex">
-              <p className="h-[40px] leading-[40px] text-base mr-5 text-gray-400">
+          <div className="h-full p-[15px] w-full flex flex-col">
+            <div>
+              <p className="h-[44px] leading-[44px] text-[16px] mr-4 text-gray-400 float-left">
                 제목 :
               </p>
-              <h1 className="text-xl h-[40px] leading-[40px]">
+              <h1 className="leading-[normal] md:text-lg text-[18px] py-2 float-left">
                 {props.webtoonInfomation?.title}
               </h1>
             </div>
-            <div className="flex leading-[40px]">
-              <p className="h-[40px] leading-[40px] text-base mr-5 text-gray-400">
+            <div>
+              <p className="h-[44px] leading-[44px] text-[16px] mr-4 text-gray-400 float-left">
                 작가 :
               </p>
-              <h1 className="text-xl h-[40px] leading-[40px]">
+              <p className=" leading-[normal] md:text-lg text-[18px] py-2  float-left">
                 {props.webtoonInfomation?.author}
-              </h1>
+              </p>
             </div>
-            <div className="flex leading-[40px]">
-              <p className="h-[40px] leading-[40px] text-base mr-5 text-gray-400">
+            <div>
+              <p className="h-[44px] leading-[44px] text-[16px] mr-4 text-gray-400 float-left">
                 플랫폼 :
               </p>
-              <h1 className="text-xl h-[40px] leading-[40px]">
+              <p className="md:text-lg text-[18px] py-2 leading-[normal] float-left">
                 {props.webtoonInfomation?.service}
-              </h1>
+              </p>
             </div>
-            <div className="flex leading-[40px]">
-              <p className="h-[40px] leading-[40px] text-base mr-5 text-gray-400">
+            <div>
+              <p className="h-[44px] leading-[44px] text-[16px] mr-4 text-gray-400 float-left">
                 연재 요일 :
               </p>
-              <h1 className="text-xl h-[40px] leading-[40px]">
+              <p className="md:text-lg text-[18px] py-2 leading-[normal] float-left">
                 {props.webtoonInfomation?.week.map((week) => {
                   if (week === 0) {
                     return "월 ";
@@ -95,7 +95,7 @@ const WebtoonDetail: React.FC<{
                   }
                   return "일 ";
                 })}
-              </h1>
+              </p>
             </div>
             <a
               href={props.webtoonInfomation?.url}
